c94dc040d894383f67cc6e1f0616c48d96ac4bc6
DomuStyle <oxgameboyxo@googlemail.com>
2024-12-11 19:22:31 +0100

    added: clouds.class.js, Clouds{}. modified: index.html, Wolrd{}, Chicken{}

---
+++ models/clouds.class.js
@@ -0,0 +1,11 @@
+class Clouds extends MovableObject {
+    y = 20; // const values
+    height = 340;
+    width = 470;
+
+    constructor() {
+        super().loadImg('./img_pollo_locco/img/5_background/layers/4_clouds/1.png');
+
+        this.x = -150 + Math.random() * 500; // spawn range starts at -150, gen. random pos. until 700
+    }
+}
--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -1,7 +1,9 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 200;
     img;
+    height = 150;
+    width = 100;
 
     loadImg(path) {
         this.img = new Image();
