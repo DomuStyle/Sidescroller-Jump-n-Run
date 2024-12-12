01163efc4216e67857f81af82211bc60676c9587
DomuStyle <oxgameboyxo@googlemail.com>
2024-12-12 19:27:52 +0100

    modified: MovableObject{loadImages()}, Character{IMAGES_WALKING[], animate()}.

--- models/character.class.js
+++ models/character.class.js
@@ -2,8 +2,33 @@ class Character extends MovableObject {
     height = 220;
     width = 100
     y = 210;
+
+    IMAGES_WALKING = [
+            './img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
+            './img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
+            './img_pollo_locco/img/2_character_pepe/2_walk/W-23.png',
+            './img_pollo_locco/img/2_character_pepe/2_walk/W-24.png',
+            './img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
+            './img_pollo_locco/img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    currentImage = 0;
+
     constructor() {
         super().loadImg('./img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+            this.animate();
+    }
+
+    animate() {
+        setInterval( ()=> {
+            let i = this.currentImage % this.IMAGES_WALKING.length; // modulu operator, creates i for each item and starts over again after reaching last item
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
+        
     }
 
     jump() {
--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -5,9 +5,27 @@ class MovableObject {
     height = 150;
     width = 100;
 
+    imageCache = {};
+
     loadImg(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+
+
+    /**
+     * 
+     * @param {Array} array - ['img/image1.png', 'img/image2.png', ....]
+     */
+
+    loadImages(array) {
+        array.forEach((path)=> {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+        
     }
 
     moveRight() {
